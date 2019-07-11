ba9d9ee5acb88d1944b30fbba50b41a4a7c5c770
Denis Kisina <kisinad@gmail.com>
2019-07-11 19:17:43 +0300

    edited css, index & js files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+   
+   // $('#reg').load("registration_form.html");
+   $("#reg").click(function(e){
+   	 e.preventDefault();
+   	 // let titles = $(".form-header").html()
+   	 // $(".content h1").html(titles)
+    $(".content").load("registration_form.html"); 
+   
+  });
+
+
+// $('#menubar').attr('href').click(function(e){
+//             e.preventDefault();
+//             $('#userpersonal').load("http://localhost/user/web/personal/index");
+//             });
+
+// $(function(){
+//     $("#reg li a").click(function(e){
+//     	e.preventDefault(); //To prevent the default anchor tag behaviour
+//         var url = this.href;
+//         $(".main").load("registration_form.html");
+//     });
+// });
+
+});
