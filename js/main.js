0a62d62949db4c1a14fcf1fa2af2261fa36f5bf5
Denis Kisina <kisinad@gmail.com>
2019-07-15 21:19:30 +0300

    added anothe link

--- js/main.js
+++ js/main.js
@@ -23,6 +23,13 @@ $(document).ready(function(){
     $(".content").load("tenant_list.html"); 
    
   });
+    $("#tenantPayment").click(function(e){
+     e.preventDefault();
+     // let titles = $(".form-header").html()
+     // $(".content h1").html(titles)
+    $(".content").load("payment.html"); 
+   
+  });
 // $('#menubar').attr('href').click(function(e){
 //             e.preventDefault();
 //             $('#userpersonal').load("http://localhost/user/web/personal/index");
