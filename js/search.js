919caaa2c95438b3716b826ae617be43dc87185e
Denis Kisina <kisinad@gmail.com>
2019-07-16 14:43:19 +0300

    added to do component'

---
+++ js/search.js
@@ -0,0 +1,37 @@
+
+var FilteredList = React.createClass({
+  filterList: function(event){
+    var updatedList = this.state.initialItems;
+    updatedList = updatedList.filter(function(item){
+      return item.toLowerCase().search(
+        event.target.value.toLowerCase()) !== -1;
+    });
+    this.setState({items: updatedList});
+  },
+  getInitialState: function(){
+     return {
+       initialItems: [
+        
+       ],
+       items: []
+     }
+  },
+  componentWillMount: function(){
+    this.setState({items: this.state.initialItems})
+  },
+  render: function(){
+    return (
+      <div className="filter-list">
+        <form>
+        <fieldset className="form-group">
+        <input type="text" className="form-control form-control-lg" placeholder="Search" onChange={this.filterList}/>
+        <button>Search</button></fieldset>
+        </form>
+      
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(<FilteredList/>, document.getElementById('search_section'));
