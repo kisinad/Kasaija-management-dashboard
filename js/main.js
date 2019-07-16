cfb2c5792e6c87921774a0676627efaf2b48afe7
Denis Kisina <kisinad@gmail.com>
2019-07-16 16:37:11 +0300

    modified main.js file

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
     $("#reg").click(function(e) {
         e.preventDefault();
-        $(".content").load("registration_form.html");
+        $(".content").load("registration_form.html"); //loads the registration form in dashboard
     });
     $("#tentReg").click(function(e) {
         e.preventDefault();
-        $(".content").load("tenant_registration.html");
+        $(".content").load("tenant_registration.html"); //loads the tenant registration page in dashboard
     });
     $("#tentlist").click(function(e) {
         e.preventDefault();
-        $(".content").load("tenant_list.html");
+        $(".content").load("tenant_list.html"); //loads tenant list page in dashboard
     });
     $("#tenantPayment").click(function(e) {
         e.preventDefault();
         $(".content").load("payment.html");
+    });
+    $("#tenant_calender").click(function(e) {
+        e.preventDefault();
+        $(".content").load("payment_calendar.html");
+    });
+    $("#newPayment").click(function(e) {
+      e.preventDefault();
+      $(".content").load("payment_status.html");
     });
 });
